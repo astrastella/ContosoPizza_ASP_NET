fb9f5cb8fd260ee9bfd603ea4b3e1a03e85e771e
astrastella <30526463+astrastella@users.noreply.github.com>
2025-03-19 11:51:47 +0000

    feat: use scoped service

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using ContosoPizza.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<IWelcomeService, WelcomeService>();
+builder.Services.AddScoped<IWelcomeService, WelcomeService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +27,11 @@ app.UseAuthorization();
 app.MapControllers();
 #endregion
 
-app.MapGet("/", (IWelcomeService welcomeService) => welcomeService.GetWelcomeMessage());
+app.MapGet("/", async (IWelcomeService welcomeService1, IWelcomeService welcomeService2) => 
+    {
+        string message1 = $"Message1: {welcomeService1.GetWelcomeMessage()}";
+        string message2 = $"Message2: {welcomeService2.GetWelcomeMessage()}";
+        return $"{message1}\n{message2}";
+    });
 
 app.Run();
